064bc08e4559ea3147c556a715304fb6b4594412
minoong <alsdndjdrktl@gmail.com>
2020-12-14 21:46:42 +0900

    경매 구현

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-const express = require("express");
-const path = require("path");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
-const passport = require("passport");
-const nunjucks = require("nunjucks");
-const dotenv = require("dotenv");
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
-//const indexRouter = require("./routes/index");
-const authRouter = require("./routes/auth");
-const { sequelize } = require("./models");
-const passportConfig = require("./passport");
+const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
+const { sequelize } = require('./models');
+const passportConfig = require('./passport');
+const sse = require('./sse');
+const webSocket = require('./socket');
 
 const app = express();
 passportConfig();
-app.set("port", process.env.PORT || 8010);
-app.set("view engine", "html");
-nunjucks.configure("views", {
+app.set('port', process.env.PORT || 8010);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
   express: app,
   watch: true,
 });
 sequelize
   .sync({ force: false })
   .then(() => {
-    console.log("데이터베이스 연결 성공");
+    console.log('데이터베이스 연결 성공');
   })
   .catch((err) => {
     console.error(err);
@@ -41,9 +43,9 @@ const sessionMiddleware = session({
   },
 });
 
-app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/img", express.static(path.join(__dirname, "uploads")));
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -51,8 +53,8 @@ app.use(sessionMiddleware);
 app.use(passport.initialize());
 app.use(passport.session());
 
-//app.use("/", indexRouter);
-app.use("/auth", authRouter);
+app.use('/', indexRouter);
+app.use('/auth', authRouter);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
@@ -62,11 +64,14 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
-app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 포트에서 대기중");
+const server = app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중');
 });
+
+webSocket(server, app);
+sse(server);
